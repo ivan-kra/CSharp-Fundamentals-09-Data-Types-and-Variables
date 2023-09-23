db3b992d74557ebc07a106b52cbf7fb2953bfa1d
Ivan Kratchanov <ivankra@gmail.com>
2023-09-23 19:37:05 +0300

    Add files via upload

---
+++ 07.WaterOverflow/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+5
+20
+100
+100 
+100
+20 
+*/
+
+int n = int.Parse(Console.ReadLine());
+int volume = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int addedLiters = int.Parse(Console.ReadLine());
+    volume += addedLiters;
+
+    if (volume > 255)
+    {
+        volume -= addedLiters;
+        Console.WriteLine("Insufficient capacity!");
+    }
+}
+
+Console.WriteLine(volume);
---
+++ 08.BeerKegs/Program.cs
@@ -0,0 +1,21 @@
+﻿string biggestKegModel = "";
+decimal MaxKegVol = 0;
+
+int kegsNum = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < kegsNum; i++)
+{
+    string kegModel = Console.ReadLine();
+    decimal kegRadius = decimal.Parse(Console.ReadLine());
+    decimal kegHeight = decimal.Parse(Console.ReadLine());
+
+    decimal kegVolume = (decimal)Math.PI * (kegRadius * kegRadius) * kegHeight;
+
+    if (MaxKegVol < kegVolume)
+    {
+        MaxKegVol = kegVolume;
+        biggestKegModel = kegModel;
+    }
+}
+
+Console.WriteLine(biggestKegModel);
