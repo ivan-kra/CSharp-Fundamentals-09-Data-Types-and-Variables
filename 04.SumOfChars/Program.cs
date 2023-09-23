ec35dc86f0ef551a25ebb47228e184e95d5a4d1d
Ivan Kratchanov <ivankra@gmail.com>
2023-09-23 19:36:12 +0300

    Add files via upload

---
+++ 04.SumOfChars/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+int totalSum = 0;
+
+for (int i = 0; i < n; i++) 
+{
+    char character = char.Parse(Console.ReadLine());
+    totalSum += character;
+}
+
+Console.WriteLine($"The sum equals: {totalSum}");
---
+++ 06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int firstLetter = 97;
+int endLetter = firstLetter + n;
+
+for (int i = firstLetter; i < endLetter; i++)
+{
+    for (int j = firstLetter; j < endLetter; j++)
+    {
+        for (int k = firstLetter; k < endLetter; k++)
+        {
+            Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+        }
+    }
+}
