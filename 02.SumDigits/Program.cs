7f11aae56252b154ba4df9881943dafc33a6e947
Ivan Kratchanov <ivankra@gmail.com>
2023-09-23 19:35:07 +0300

    Add files via upload

---
+++ 02.SumDigits/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (num != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+
+Console.WriteLine(sum);
---
+++ 03.Elevator/Program.cs
@@ -0,0 +1,6 @@
+﻿int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+
+int courses = (int)Math.Ceiling((double)n / p);
+
+Console.WriteLine(courses);
